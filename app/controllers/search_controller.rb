1113392cdbc06004b92c6c08a794675d31083e73
aaerman <aaerman@hotmail.com>
2009-04-24 16:15:45 -0700

    more geocoding tests5

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,19 +6,23 @@ class SearchController < ApplicationController
 
   def index                                          
     #@search_results = Business.located_at("%"+params[:location]+"%").named("%"+params[:search_string]+"%").in_category(params[:category_id])    
-    @search_results = Business.find(:all, :origin => params[:location], :within => 25)
+    if RAILS_ENV == "production" then @search_results = Business.named("%"+params[:search_string]+"%").in_category(params[:category_id]).find(:all, :origin => params[:location], :within => 25)
+    else @search_results = Business.located_at("%"+params[:location]+"%").named("%"+params[:search_string]+"%").in_category(params[:category_id]) 
+    end
     #.named("%"+params[:search_string]+"%").in_category(params[:category_id])    
     @category_list = Category.find(:all)  
     
     # The following local vars are used to pass on search parameters to filter requests.
     @search_string = params[:search_string]
     @location = params[:location]
-# Need to modify this to center on searched location
-    #@search_center={:latitude => 37.700, :longitude => 237.750}
+    
+    #Geocode search location if rails env is production otherwise use a default location
     geo=Geokit::Geocoders::MultiGeocoder.geocode(params[:location])
     errors.add(:address, "Could not Geocode address") if !geo.success
-    @search_center={:latitude => geo.lat,  :longitude => geo.lng}
-#    @markers = [[-34.2023, 18.3794], [-34.2029, 18.3797], [-34.2022, 18.3811],[-34.2016, 18.3829], [-34.2006, 18.3849]].collect
+    @search_center={:latitude => geo.lat,  :longitude => geo.lng} 
+    #dummy center @search_center={:latitude =>37.757763,  :longitude => -121.964991}
+    
+    #Create markers array from search results
     items = [*@search_results]
     @markers =  items.collect do |item|
          [item.lat, item.lng]
@@ -27,12 +31,3 @@ class SearchController < ApplicationController
 
 end
 
-
-  def geocode_address
-    address= [address_line_1, address_line_2, city, state_region, zip_code, country].join(', ')
-    logger.info(address)
-    #address='31 tareyton Ct,, San Ramon, CA, 94583,'
-    geo=Geokit::Geocoders::MultiGeocoder.geocode(address)
-    errors.add(:address, "Could not Geocode address") if !geo.success
-    self.lat, self.lng = geo.lat,geo.lng if geo.success
-  end
