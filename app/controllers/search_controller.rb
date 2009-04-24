2c7751c95865b9039ff424fdf9c8af14922d7f72
aaerman <aaerman@hotmail.com>
2009-04-24 14:53:57 -0700

    more geocoding tests2

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -15,7 +15,9 @@ class SearchController < ApplicationController
     @location = params[:location]
 # Need to modify this to center on searched location
     #@search_center={:latitude => 37.700, :longitude => 237.750}
-     @search_center={params[:location]}
+    geo=Geokit::Geocoders::MultiGeocoder.geocode(params[:location])
+    errors.add(:address, "Could not Geocode address") if !geo.success
+    @search_center=geo
 #    @markers = [[-34.2023, 18.3794], [-34.2029, 18.3797], [-34.2022, 18.3811],[-34.2016, 18.3829], [-34.2006, 18.3849]].collect
     items = [*@search_results]
     @markers =  items.collect do |item|
@@ -24,3 +26,13 @@ class SearchController < ApplicationController
   end
 
 end
+
+
+  def geocode_address
+    address= [address_line_1, address_line_2, city, state_region, zip_code, country].join(', ')
+    logger.info(address)
+    #address='31 tareyton Ct,, San Ramon, CA, 94583,'
+    geo=Geokit::Geocoders::MultiGeocoder.geocode(address)
+    errors.add(:address, "Could not Geocode address") if !geo.success
+    self.lat, self.lng = geo.lat,geo.lng if geo.success
+  end
